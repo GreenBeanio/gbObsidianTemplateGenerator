ef2ffda7016293a34e40b6aebf3e3ac5be11fc2e
Garrett Johnson <pingujohnson@gmail.com>
2026-09-12 04:26:44 -0400

    exp: Experimenting with periods
    Weeks seem like they are going to be much harder to implement than the other periods

--- Functions.R
+++ Functions.R
@@ -1,12 +1,19 @@
 #####
 #####
 #####
-createYear <- \(input_date, template, unique_timestamp = Sys.time(), header_func = makeObsidianFilePath) {
+createYear <- \(input_date, 
+                template_dir,
+                template_pre = "",
+                unique_timestamp = Sys.time(), 
+                header_func = makeObsidianFilePath) {
   if(length(input_date) != 1) {
     stop("input_date needs a single date")
   }
-  # This is probably wasteful if looping. Should load this once.
-  loaded_template <- readr::read_file(template)
+  template_file <- "Year.md"
+  if(template_pre != "") {
+    template_file <- glue::glue("{template_pre}{template_file}")
+  }
+  loaded_template <- readr::read_file(file.path(template_dir, template_file))
   c_year <- lubridate::year(input_date)
   output_list <- list()
   # Get the constant variables
@@ -24,11 +31,8 @@ createYear <- \(input_date, template, unique_timestamp = Sys.time(), header_func
     return(output_list)
   }
   # Load template and replace the constant variables
-  new_template <- loaded_template
-  new_template <- new_template |>
-    stringr::str_replace_all("\\{title\\}", c_title) |>
-    stringr::str_replace_all("\\{creation_date\\}", c_creation_date) |>
-    stringr::str_replace_all("\\{unique_timestamp\\}", c_unique_timestamp)
+  loaded_template <- loaded_template |> 
+    obsidianMetadataReplace(c_title, c_creation_date, c_unique_timestamp)
   # Run the quarterly results to get the needed links
   quarter_dates <- c(
     lubridate::make_date(c_year, 1, 1),
@@ -36,18 +40,19 @@ createYear <- \(input_date, template, unique_timestamp = Sys.time(), header_func
     lubridate::make_date(c_year, 7, 1),
     lubridate::make_date(c_year, 10, 1))
   quarters <- purrr::map(quarter_dates, 
-                         \(x) createQuarter(x, 
-                                            file.path(template_dir, 
-                                                      "Quarter.md"),
-                                            unique_timestamp)) |> 
+                         \(x) createQuarter(input_date =  x,
+                                            template_dir = template_dir,
+                                            template_pre = template_pre,
+                                            unique_timestamp = unique_timestamp,
+                                            header_func = header_func)) |> 
     purrr::flatten() |>
     header_func()
-  new_template <- replaceHeaderLinks(
+  loaded_template <- replaceHeaderLinks(
     quarters,
     paste0("Quarter ", 1:4),
-    new_template)
+    loaded_template)
   # Write the file and add to output
-  readr::write_file(new_template, c_output)
+  readr::write_file(loaded_template, c_output)
   output_list[[output_name]] <- c_output
   return(output_list)
 }
@@ -55,12 +60,19 @@ createYear <- \(input_date, template, unique_timestamp = Sys.time(), header_func
 #####
 #####
 #####
-createQuarter <- \(input_date, template, unique_timestamp = Sys.time()) {
+createQuarter <- \(input_date, 
+                   template_dir,
+                   template_pre = "",
+                   unique_timestamp = Sys.time(), 
+                   header_func = makeObsidianFilePath) {
   if(length(input_date) != 1) {
     stop("input_date needs a single date")
   }
-  # This is probably wasteful if looping. Should load this once.
-  loaded_template <- readr::read_file(template)
+  template_file <- "Quarter.md"
+  if(template_pre != "") {
+    template_file <- glue::glue("{template_pre}{template_file}")
+  }
+  loaded_template <- readr::read_file(file.path(template_dir, template_file))
   c_quarter <- lubridate::quarter(input_date)
   c_year <- lubridate::year(input_date)
   output_list <- list()
@@ -79,17 +91,28 @@ createQuarter <- \(input_date, template, unique_timestamp = Sys.time()) {
     return(output_list)
   }
   # Load template and replace the constant variables
-  new_template <- loaded_template
-  new_template <- new_template |>
-    stringr::str_replace_all("\\{title\\}", c_title) |>
-    stringr::str_replace_all("\\{creation_date\\}", c_creation_date) |>
-    stringr::str_replace_all("\\{unique_timestamp\\}", c_unique_timestamp)
-  # Run the monthly results to get the needed links
-  ###########
-  ###########
-  ###########
+  loaded_template <- loaded_template |> 
+    obsidianMetadataReplace(c_title, c_creation_date, c_unique_timestamp)
+  # Run the months
+  start_month <- lubridate::month(lubridate::floor_date(input_date, unit = "quarter"))
+  month_dates <- c(
+    lubridate::make_date(c_year, start_month, 1),
+    lubridate::make_date(c_year, start_month + 1, 1),
+    lubridate::make_date(c_year, start_month + 2, 1))
+  c_months <- purrr::map(month_dates, 
+                         \(x) createMonth(input_date =  x,
+                                          template_dir = template_dir,
+                                          template_pre = template_pre,
+                                          unique_timestamp = unique_timestamp,
+                                          header_func = header_func)) |> 
+    purrr::flatten() |>
+    header_func()
+  loaded_template <- replaceHeaderLinks(
+    c_months,
+    paste0("Month ", 1:3),
+    loaded_template)
   # Write the file and add to output
-  readr::write_file(new_template, c_output)
+  readr::write_file(loaded_template, c_output)
   output_list[[output_name]] <- c_output
   return(output_list)
 }
@@ -97,12 +120,19 @@ createQuarter <- \(input_date, template, unique_timestamp = Sys.time()) {
 #####
 #####
 #####
-createMonth <- \(input_date, template, unique_timestamp = Sys.time()) {
+createMonth <- \(input_date, 
+                 template_dir,
+                 template_pre = "",
+                 unique_timestamp = Sys.time(), 
+                 header_func = makeObsidianFilePath) {
   if(length(input_date) != 1) {
     stop("input_date needs a single date")
   }
-  # This is probably wasteful if looping. Should load this once.
-  loaded_template <- readr::read_file(template)
+  template_file <- "Month.md"
+  if(template_pre != "") {
+    template_file <- glue::glue("{template_pre}{template_file}")
+  }
+  loaded_template <- readr::read_file(file.path(template_dir, template_file))
   c_month <- lubridate::month(input_date)
   c_year <- lubridate::year(input_date)
   quarter_month <- lubridate::month(lubridate::floor_date(input_date, unit = "quarter"))
@@ -122,17 +152,33 @@ createMonth <- \(input_date, template, unique_timestamp = Sys.time()) {
     return(output_list)
   }
   # Load template and replace the constant variables
-  new_template <- loaded_template
-  new_template <- new_template |>
-    stringr::str_replace_all("\\{title\\}", c_title) |>
-    stringr::str_replace_all("\\{creation_date\\}", c_creation_date) |>
-    stringr::str_replace_all("\\{unique_timestamp\\}", c_unique_timestamp)
-  # Run the monthly results to get the needed links
-  ###########
-  ###########
-  ###########
+  loaded_template <- loaded_template |> 
+    obsidianMetadataReplace(c_title, c_creation_date, c_unique_timestamp)
+  # Run the weeks
+  start_month_day <- lubridate::floor_date(input_date, unit = "month")
+  start_week <- lubridate::isoweek(start_month_day)
+  end_week <- lubridate::isoweek(lubridate::ceiling_date(input_date, 
+                                                         unit = "month") - 
+                                   lubridate::days(1))
+  week_count <- end_week - start_week + 1
+  start_day_off <- (start_month_day |> lubridate::wday(week_start = 1)) - 1
+  first_week_day <- start_month_day - lubridate::days(start_day_off)
+  week_dates <- purrr::map2_vec(1:week_count, first_week_day,
+                                \(c_wk, sd) sd + lubridate::weeks(c_wk - 1))
+  c_weeks <- purrr::map(week_dates, 
+                        \(x) createWeek(input_date =  x,
+                                        template_dir = template_dir,
+                                        template_pre = template_pre,
+                                        unique_timestamp = unique_timestamp,
+                                        header_func = header_func)) |> 
+    purrr::flatten() |>
+    header_func()
+  loaded_template <- replaceHeaderLinks(
+    quarters,
+    paste0("Week ", 1:length(c_weeks)),
+    loaded_template)
   # Write the file and add to output
-  readr::write_file(new_template, c_output)
+  readr::write_file(loaded_template, c_output)
   output_list[[output_name]] <- c_output
   return(output_list)
 }
@@ -140,15 +186,29 @@ createMonth <- \(input_date, template, unique_timestamp = Sys.time()) {
 #####
 #####
 #####
-createWeek <- \(input_date, template, unique_timestamp = Sys.time()) {
+createWeek <- \(input_date, 
+                template_dir,
+                template_pre = "",
+                unique_timestamp = Sys.time(), 
+                header_func = makeObsidianFilePath) {
   if(length(input_date) != 1) {
     stop("input_date needs a single date")
   }
-  # This is probably wasteful if looping. Should load this once.
-  loaded_template <- readr::read_file(template)
-  c_week <- lubridate::week(input_date)
+  template_file <- "Week.md"
+  if(template_pre != "") {
+    template_file <- glue::glue("{template_pre}{template_file}")
+  }
+  loaded_template <- readr::read_file(file.path(template_dir, template_file))
+  c_week <- lubridate::isoweek(input_date)
   c_year <- lubridate::year(input_date)
-  month_week <- lubridate::week(lubridate::floor_date(input_date, unit = "month"))
+  # Need to modify this to be special since one month it could be week 1 and in another it could be week 5,  etc.
+  # Probably best to handle this based on the input from the month actually.
+  # Huh, I may need to sadly add another parameter to the week for this.
+  # That or handle it differently for the weeks since they are variable unlike
+  # the other parameters.
+  # Huh, damn there's also the issue with if it's the first or last month of the
+  # year the weeks aren't guaranteed to be "full".
+  month_week <- lubridate::isoweek(lubridate::floor_date(input_date, unit = "month"))
   output_list <- list()
   # Get the constant variables
   c_title <- glue::glue("{c_year}_{c_week}")
@@ -165,17 +225,28 @@ createWeek <- \(input_date, template, unique_timestamp = Sys.time()) {
     return(output_list)
   }
   # Load template and replace the constant variables
-  new_template <- loaded_template
-  new_template <- new_template |>
-    stringr::str_replace_all("\\{title\\}", c_title) |>
-    stringr::str_replace_all("\\{creation_date\\}", c_creation_date) |>
-    stringr::str_replace_all("\\{unique_timestamp\\}", c_unique_timestamp)
-  # Run the daily results to get the needed links
-  ###########
-  ###########
-  ###########
+  loaded_template <- loaded_template |> 
+    obsidianMetadataReplace(c_title, c_creation_date, c_unique_timestamp)
+  # Run the quarterly results to get the needed links
+  quarter_dates <- c(
+    lubridate::make_date(c_year, 1, 1),
+    lubridate::make_date(c_year, 4, 1),
+    lubridate::make_date(c_year, 7, 1),
+    lubridate::make_date(c_year, 10, 1))
+  quarters <- purrr::map(quarter_dates, 
+                         \(x) createQuarter(input_date =  x,
+                                            template_dir = template_dir,
+                                            template_pre = template_pre,
+                                            unique_timestamp = unique_timestamp,
+                                            header_func = header_func)) |> 
+    purrr::flatten() |>
+    header_func()
+  loaded_template <- replaceHeaderLinks(
+    quarters,
+    paste0("Quarter ", 1:4),
+    loaded_template)
   # Write the file and add to output
-  readr::write_file(new_template, c_output)
+  readr::write_file(loaded_template, c_output)
   output_list[[output_name]] <- c_output
   return(output_list)
 }
@@ -183,12 +254,19 @@ createWeek <- \(input_date, template, unique_timestamp = Sys.time()) {
 #####
 #####
 #####
-createDay <- \(input_date, template, unique_timestamp = Sys.time()) {
+createDay <- \(input_date, 
+               template_dir,
+               template_pre = "",
+               unique_timestamp = Sys.time(), 
+               header_func = makeObsidianFilePath) {
   if(length(input_date) != 1) {
     stop("input_date needs a single date")
   }
-  # This is probably wasteful if looping. Should load this once.
-  loaded_template <- readr::read_file(template)
+  template_file <- "Day.md"
+  if(template_pre != "") {
+    template_file <- glue::glue("{template_pre}{template_file}")
+  }
+  loaded_template <- readr::read_file(file.path(template_dir, template_file))
   input_date <- lubridate::date(input_date)
   output_list <- list()
   # Get the constant variables
@@ -207,13 +285,10 @@ createDay <- \(input_date, template, unique_timestamp = Sys.time()) {
     return(output_list)
   }
   # Load template and replace the constant variables
-  new_template <- loaded_template
-  new_template <- new_template |>
-    stringr::str_replace_all("\\{title\\}", c_title) |>
-    stringr::str_replace_all("\\{creation_date\\}", c_creation_date) |>
-    stringr::str_replace_all("\\{unique_timestamp\\}", c_unique_timestamp)
+  loaded_template <- loaded_template |> 
+    obsidianMetadataReplace(c_title, c_creation_date, c_unique_timestamp)
   # Write the file and add to output
-  readr::write_file(new_template, output_file)
+  readr::write_file(loaded_template, output_file)
   output_list[[output_name]] <- output_file
   return(output_list)
 }
--- Utils.R
+++ Utils.R
@@ -29,3 +29,16 @@ makeObsidianFilePath <- \(list_of_paths) {
                    basename() |> 
                    htmltools::urlEncodePath())) 
 }
+
+#####
+#####
+#####
+obsidianMetadataReplace <- \(template,
+                             title,
+                             creation_date,
+                             unique_timestamp) {
+  return(template |>
+           stringr::str_replace_all("\\{title\\}", title) |>
+           stringr::str_replace_all("\\{creation_date\\}", creation_date) |>
+           stringr::str_replace_all("\\{unique_timestamp\\}", unique_timestamp))
+}
