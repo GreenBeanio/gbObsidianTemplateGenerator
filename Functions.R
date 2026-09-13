71eaf761aece5cdb4a92cafb11de5412f7848a3b
Garrett Johnson <pingujohnson@gmail.com>
2026-09-13 03:03:25 -0400

    exp: Month fix earlier dates
    This does fix it making dates from previous months, but I think this is actually flawed and should only do so on the first week of the year

--- Functions.R
+++ Functions.R
@@ -153,7 +153,7 @@ createMonth <- \(input_date,
   quarter_month <- lubridate::month(lubridate::floor_date(input_date, unit = "quarter"))
   output_list <- list()
   # Get the constant variables
-  c_title <- glue::glue("{c_year}_{c_month |> stringr::str_pad(2, 'left', '0')}}")
+  c_title <- glue::glue("{c_year}_{c_month |> stringr::str_pad(2, 'left', '0')}")
   c_creation_date <- strftime(unique_timestamp, "%Y-%m-%d")
   c_unique_timestamp <- strftime(unique_timestamp, "%Y%m%d%H%M%S")
   # Get the output path and check if it exists
@@ -181,10 +181,20 @@ createMonth <- \(input_date,
                                                          unit = "month") - 
                                    lubridate::days(1))
   week_count <- end_week - start_week + 1
-  start_day_off <- (start_month_day |> lubridate::wday(week_start = 1)) - 1
-  first_week_day <- start_month_day - lubridate::days(start_day_off)
-  week_dates <- purrr::map2_vec(1:week_count, first_week_day,
-                                \(c_wk, sd) sd + lubridate::weeks(c_wk - 1))
+  start_week_day <- start_month_day |> lubridate::wday(week_start = 1)
+  start_day_off <- 0
+  week_dates <- lubridate::Date()
+  if(start_week_day != 1) {
+    start_day_off <- 7 - start_week_day + 1
+    first_monday <- input_date + lubridate::days(start_day_off)
+    week_dates <- purrr::map2_vec(2:week_count, 
+                                  first_monday,
+                                  \(c_wk, sd) sd + lubridate::weeks(c_wk - 1))
+    week_dates <- c(start_month_day, week_dates)
+  } else {
+    week_dates <- purrr::map2_vec(1:week_count, start_month_day,
+                                  \(c_wk, sd) sd + lubridate::weeks(c_wk - 1))
+  }
   c_weeks <- purrr::map(week_dates, 
                         \(x) createWeek(input_date =  x,
                                         template_dir = template_dir,
