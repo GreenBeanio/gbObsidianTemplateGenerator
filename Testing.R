dff30f8a1bb38fa01b18b38e5e9a72d1c74cb433
Garrett Johnson <pingujohnson@gmail.com>
2026-09-19 10:35:59 -0400

    chore: Update temporary testing script
    I will replace this script with proper tests soon

--- Testing.R
+++ Testing.R
@@ -1,10 +1,11 @@
-for(sf in dir(file.path(here::here(), "R"), ".R", full.names = T)) {
-  source(sf)
-}
+# for(sf in dir(file.path(here::here(), "R"), ".R", full.names = T)) {
+#   source(sf)
+# }
+devtools::load_all()
 
 input_dir <- here::here()
 output_dir <- file.path(input_dir, "Output")
-template_dir <- file.path(input_dir, "Templates")
+template_dir <- file.path(input_dir, "inst", "extdata", "Templates")
 
 unlink(file.path(output_dir, "*"), recursive = T, force = T)
 
@@ -14,15 +15,15 @@ createYear(input_date = test_date,
            template_dir = template_dir,
            output_dir = output_dir,
            author = "Garrett Johnson")
-# 
+#
 # createQuarter(input_date = test_date,
 #            template_dir = template_dir,
 #            output_dir = output_dir)
-# 
+#
 # createMonth(input_date = test_date,
 #            template_dir = template_dir,
 #            output_dir = output_dir)
-# 
+#
 # createWeek(input_date = test_date,
 #            template_dir = template_dir,
 #            output_dir = output_dir)
